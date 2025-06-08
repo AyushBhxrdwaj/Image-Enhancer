62c57b95ebcd5daaca5511cac67e40e6d1cd303e
AyushBhxrdwaj <ayush.ds321@gmail.com>
2025-06-08 14:07:01 +0530

    repo created

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Upload from './Upload'
+import Preview from './Preview'
+
+const Home = () => {
+    return (
+        <div className=''>
+
+            <Upload />
+            <Preview />
+        </div>
+    )
+}
+
+export default Home
